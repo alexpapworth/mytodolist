8167265fe385d415875587ec1989d150dc25ba06
Alex Papworth <arpapworth@gmail.com>
2019-11-16 20:57:25 +0000

    Add button to create new todo item
    We read from the existing todo items to determine what the next colour and data-name should be.
    
     - The colour is taken from the next colour along from the final todo. I.E. red is proceeded by yellow
     - The data-name is taken from the first available name. We test this by querying each possible data-name on the DOM
    
    Both of these features are built with the expection of being able to delete todos in the future. Deleting would remove the entry from the DOM, and clear down it's localStorage data, allowing it's data-name to be reassigned.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var container;
 var dragging;
 var dropping;
 
+var colors = ["red", "orange", "yellow", "limegreen", "green", "aqua", "blue", "purple"]
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 function dragstart(e) {
   dragging = this;
   dragging.classList.add('dragging');
@@ -77,12 +80,65 @@ function loadContent() {
   }
 }
 
+function createTodo() {
+  if (document.querySelectorAll('.input').length == 0) {
+    var countOfTodos = 0;
+    var chosenLetter = "a";
+  }
+  else {
+    var countOfTodos = document.querySelectorAll('.input').length;
+    var findExistingLetter = ""
+    var chosenLetter = ""
+    var numberOfCharacters = (Math.floor(countOfTodos / 26) +1)
+
+    for (var i = 0; i <= countOfTodos; i++) {
+
+      findExistingLetter = ""
+
+      for (var j = 0; j < numberOfCharacters; j++) {
+        findExistingLetter += letters[i]
+      }
+
+      if (!document.querySelector(`.input[data-name="${findExistingLetter}"`)) {
+        chosenLetter = findExistingLetter;
+        i = countOfTodos;
+      }
+    }
+
+    var lastColor = document.querySelectorAll('.input')[countOfTodos-1].id;
+
+    if (!lastColor) {
+      lastColor = "red";
+    }
+
+    var colorIndex = colors.indexOf(lastColor)+1
+
+    if (colorIndex > 7) {
+      colorIndex = 0;
+    }
+  }
+
+  var todo = document.createElement('div');
+
+  todo.classList.add("input", colors[colorIndex]);
+  todo.id = colors[colorIndex];
+  todo.dataset.name = chosenLetter;
+  todo.contentEditable = true;
+  todo.draggable = true;
+
+  container.appendChild(todo);
+  ready();
+
+  window.scrollTo(0,document.body.scrollHeight);
+}
+
 var ready;
 
 ready = function() {
   loadContent();
 
   container = document.querySelector('#items');
+  document.querySelector('.new-todo').addEventListener("click", createTodo);
 
   for (var i = 0; i < document.querySelectorAll('.input').length; i++) {
 
